3fda4dee9cd8a7644043efd3b0fcfc5a54845d20
MaimunaMaigari <165202506+MaimunaMaigari@users.noreply.github.com>
2024-03-27 19:39:29 +0100

    Add files via upload

---
+++ maimuna umar maigari.rb
@@ -0,0 +1,9 @@
+name = "Maimuna Umar Maigari"
+email = "maimunaumarmaigari@gmail.com"
+slack_username = "@madamglobal.slack.com"
+area_of_interest = "cancer genomics"
+
+puts"Name: #{name}"
+puts"Email:#{email}"
+puts"Slack Username:#{slack_username}"
+puts"Area of interest in Bioinformatics:#{area_of_interest}"
